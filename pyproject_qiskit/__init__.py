0a0a6f7aa3cffff2be2c8808c39e89455123d982
Pedro Rivero <pedro.rivero.ramirez@gmail.com>
2024-01-09 06:56:01 -0500

    docs: update copyright year

--- pyproject_qiskit/__init__.py
+++ pyproject_qiskit/__init__.py
@@ -1,6 +1,6 @@
 # This code is part of Qiskit.
 #
-# (C) Copyright IBM 2023.
+# (C) Copyright IBM 2024.
 #
 # This code is licensed under the Apache License, Version 2.0. You may
 # obtain a copy of this license in the LICENSE.txt file in the root directory
@@ -12,7 +12,7 @@
 
 """Qiskit-based Python project."""
 
-__copyright__ = "(C) Copyright IBM 2023"
+__copyright__ = "(C) Copyright IBM 2024"
 __version__ = "0.0.0"
 
 
